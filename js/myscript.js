f5a94e97a10c89409ea4238d5e190306d49493e6
NikitaSmygin <30476122+NikitaSmygin@users.noreply.github.com>
2017-10-12 05:02:06 +0300

    js

--- js/myscript.js
+++ js/myscript.js
@@ -9,28 +9,49 @@ var submit = modal.querySelector(".button-2");
 var error1 = modal.querySelector('.error-name');
 var error2 = modal.querySelector('.error-email');
 var error3 = modal.querySelector('.error-write');
+var color1 = modal.querySelector('.color1');
+var color2 = modal.querySelector('.color2');
+var color3 = modal.querySelector('.color3');
+var storage = localStorage.getItem("name_field");
 
 link.addEventListener("click", function () {
     modal.classList.add("open");
-    name_field.focus();
+    if(storage){
+        name_field.value = storage;
+        email.focus();
+    } else {
+        name_field.focus();
+    };
+    
 });
 closeModal.addEventListener('click', function () {
-    modal.classList.remove('open')
+    modal.classList.remove('open');
 });
 
 form.addEventListener('submit', function (e) {
     e.preventDefault();
     if (!name_field.value) {
-        error1.classList.add("error")
+        error1.classList.add("error");
+        color1.classList.add("color");
     };
     if (!email.value) {
-        error2.classList.add("error")
+        error2.classList.add("error");
+        color2.classList.add("color");
     };
     if (!text.value) {
-        error3.classList.add("error")
+        error3.classList.add("error");
+        color3.classList.add("color");
+    } else {
+        localStorage.setItem("name", name_field.value);
+        localStorage.setItem("e-mail", email_field.value);
     };
-
 });
+
+window.addEventListener('keydown',function(e) {
+    if(e.keyCode ===27) {
+        modal.classList.remove('open');
+    }
+})
 
 
 
